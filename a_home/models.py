43b1a508a92997e6a9a8ad3d37a2ef8d63c4b233
odmoreno <oscarpol95@gmail.com>
2025-04-15 12:21:58 -0500

    init

---
+++ a_home/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from colorfield.fields import ColorField
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class SiteSetting(models.Model):
+    name = models.CharField(max_length=100)
+    color = ColorField(default="#000000")
+
+    def __str__(self):
+        return self.name
